11afa8b1678558677f4284a15240c9dac9641c7b
John Reeme <john.reeme@soteradefense.com>
2017-01-02 08:54:23 -0600

    About to branch to vita-auth2

--- server/server.ts
+++ server/server.ts
@@ -19,10 +19,14 @@
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
   boot(app, __dirname, function (err) {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    }
 
     // start the server if `$ node server.js`
-    if (require.main === module) app.start();
+    if (require.main === module) {
+      app.start();
+    }
   });
 })();
 
