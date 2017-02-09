d6d31da7a9458295857f83178fd23858e5ffc2a9
John Reeme <john.reeme@soteradefense.com>
2017-02-09 14:48:34 -0600

    Patched up a few things to make it work again.

--- common/modelClasses/etl-flow.ts
+++ common/modelClasses/etl-flow.ts
@@ -3,76 +3,75 @@ import {EtlBase} from "./etl-base";
 import * as _ from 'lodash';
 const async = require('async');
 export class EtlFlow extends EtlBase {
-  etlSteps: EtlStep[];
+    etlSteps: EtlStep[];
 
-  private constructor(protected loopbackModelInstance: any = null) {
-    super(EtlBase.server.models.EtlFlow, {include: ['steps']}, loopbackModelInstance);
-  }
+    private constructor(protected loopbackModelInstance: any = null) {
+        super(EtlBase.server.models.EtlFlow, {include: ['steps']}, loopbackModelInstance);
+    }
 
-  set expanded(newExpanded) {
-    this.typeScriptObject.expanded = newExpanded;
-  }
+    set expanded(newExpanded) {
+        this.typeScriptObject.expanded = newExpanded;
+    }
 
-  get expanded(): string {
-    return this.typeScriptObject.expanded;
-  }
+    get expanded(): string {
+        return this.typeScriptObject.expanded;
+    }
 
-  set etlFileId(newEtlFileId) {
-    this.typeScriptObject.etlFileId = newEtlFileId;
-  }
+    set etlFileId(newEtlFileId) {
+        this.typeScriptObject.etlFileId = newEtlFileId;
+    }
 
-  get etlFileId(): string {
-    return this.typeScriptObject.etlFileId;
-  }
+    get etlFileId(): string {
+        return this.typeScriptObject.etlFileId;
+    }
 
-  set lastStatus(newLastStatus) {
-    this.typeScriptObject.lastStatus = newLastStatus;
-  }
+    set lastStatus(newLastStatus) {
+        this.typeScriptObject.lastStatus = newLastStatus;
+    }
 
-  get lastStatus(): string {
-    return this.typeScriptObject.lastStatus;
-  }
+    get lastStatus(): string {
+        return this.typeScriptObject.lastStatus;
+    }
 
-  get steps(): EtlStep[] {
-    return this.etlSteps;
-  }
+    get steps(): EtlStep[] {
+        return this.etlSteps;
+    }
 
-  private loadEtlSteps(cb: (err, flows?) => void) {
-    let me = this;
-    let fnArray = [];
-    me.typeScriptObject.steps.forEach(step => {
-      fnArray.push(async.apply((step, cb) => {
-        EtlStep.findById(step.id, (err, etlStep) => {
-          cb(err, etlStep);
+    private loadEtlSteps(cb: (err, flows?) => void) {
+        let me = this;
+        let fnArray = [];
+        me.typeScriptObject.steps.forEach(step => {
+            fnArray.push(async.apply((step, cb) => {
+                EtlStep.findById(step.id, (err, etlStep) => {
+                    cb(err, etlStep);
+                });
+            }, step));
         });
-      }, step));
-    });
-    async.parallel(fnArray, (err, results) => {
-      cb(err, me.etlSteps = results);
-    });
-  }
-
-  createStep(typeScriptObject, cb: (err, etlStep: EtlStep) => void) {
-    let me = this;
-    this._createHasManyInstance('steps', typeScriptObject, (err, etlStep) => {
-      me.loadEtlSteps(err => {
-        let newEtlStep = _.find(me.etlSteps, s => {
-          return etlStep.id.toString() === s.id;
+        async.parallel(fnArray, (err, results) => {
+            cb(err, me.etlSteps = results);
         });
-        cb(err, newEtlStep);
-      });
-    });
-  }
+    }
 
-  static createFromLoopbackInstance(loopbackInstance, cb: (err, etlFlow: EtlFlow) => void) {
-    let newEtlFlow = new EtlFlow(loopbackInstance);
-    cb(null, newEtlFlow);
-    /*    newEtlFlow.loadEtlSteps(err => {
-     cb(err, newEtlFlow);
-     });*/
-  }
+    createStep(typeScriptObject, cb: (err, etlStep: EtlStep) => void) {
+        let me = this;
+        this._createHasManyInstance('steps', typeScriptObject, (err, etlStep) => {
+            me.loadEtlSteps(err => {
+                let newEtlStep = _.find(me.etlSteps, s => {
+                    return etlStep.id.toString() === s.id;
+                });
+                cb(err, newEtlStep);
+            });
+        });
+    }
 
-  static findById(id, cb: (err, etlFlow: EtlFlow) => void) {
-    new EtlFlow()._findById(id, cb);
-  }
+    static createFromLoopbackInstance(loopbackInstance, cb: (err, etlFlow: EtlFlow) => void) {
+        let newEtlFlow = new EtlFlow(loopbackInstance);
+        newEtlFlow.loadEtlSteps(err => {
+            cb(err, newEtlFlow);
+        });
+    }
+
+    static findById(id, cb: (err, etlFlow: EtlFlow) => void) {
+        new EtlFlow()._findById(id, cb);
+    }
 }
