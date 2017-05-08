ffd9d3860e0c3810318beb2a5b076598b091dfbe
nobody.nobody <nobody@nowhere.com>
2017-05-08 13:52:04 -0500

    Support config route

--- server/boot/config-help.ts
+++ server/boot/config-help.ts
@@ -1,5 +1,5 @@
 module.exports = function (server) {
-  server.get('/auth/update-user-info', function (req, res) {
+  server.get('/config', function (req, res) {
     return res.status(200).send('How Now Brown Cow');
   });
 };
