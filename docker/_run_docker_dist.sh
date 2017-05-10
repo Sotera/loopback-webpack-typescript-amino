b1b09334f167cdfbee7b99e2bc4bd700010c6a87
nobody.nobody <nobody@nowhere.com>
2017-05-10 10:16:30 -0500

    Fix strongloop deploy

--- docker/_run_docker_dist.sh
+++ docker/_run_docker_dist.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -dt --name vita -h vita -p 8701:8701 -p 8080:8080 jreeme/vita:dist-04-APR-2017
+docker run -dt --name vita -h vita -p 8701:8701 -p 8080:8080 jreeme/vita:dist-09-MAY-2017
